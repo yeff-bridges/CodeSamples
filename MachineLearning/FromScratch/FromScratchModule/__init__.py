68627581697f6d811f4d749cf146eeef0fc86680
Jeremy Frechette <41272374+yeff-bridges@users.noreply.github.com>
2020-04-17 19:30:28 -0400

    FromScratchModule/__init__.py now has Activation

--- MachineLearning/FromScratch/FromScratchModule/__init__.py
+++ MachineLearning/FromScratch/FromScratchModule/__init__.py
@@ -1,8 +1,11 @@
+import FromScratchModule.Activation.Function
+import FromScratchModule.Activation.Class
 import FromScratchModule.NNet
 import FromScratchModule.Trees
 import FromScratchModule.Loss
 import FromScratchModule.Optimizers
 import FromScratchModule.Performance
 import FromScratchModule.Toy
+import FromScratchModule.Regression
 
 name = "FromScratch"
